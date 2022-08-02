54f924a527b92a218345e0093e29d7cb25171aa7
I. Fritsch <ines.fritsch@gmail.com>
2022-08-02 22:41:54 +0200

    bind and output note data

--- src/app/note/note.component.ts
+++ src/app/note/note.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-note',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./note.component.scss']
 })
 export class NoteComponent implements OnInit {
+
+  @Input() title!: string;
+  @Input() body!: string;
 
   constructor() { }
 
