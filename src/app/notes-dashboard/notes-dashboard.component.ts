6c7f9ede785148cf45eab9c93d5c0c17a0a152bd
I. Fritsch <ines.fritsch@gmail.com>
2022-08-04 13:37:40 +0200

    move note to trash

--- src/app/notes-dashboard/notes-dashboard.component.ts
+++ src/app/notes-dashboard/notes-dashboard.component.ts
@@ -7,29 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotesDashboardComponent implements OnInit {
 
-  notes: any[] = 
+  notes = 
   [
     {title: "Example note title", body: "This is the content of your note. Click on the form above and add a new one. If you click on delete the note will be moved to the trash folder, from which you can restore the note or delete it permanently.", id:1},
-    {title: "Delegation", body:"Q.: How many programmers does it take to change a light bulb? A.: Null – This is a hardware problem.", id:2},
+    {title: "Delegation", body:"Q.: How many programmers does it take to change a light bulb? <br> A.: Null – This is a hardware problem.", id:2},
   ];
 
-  trash!: any[];
+  trash: {title: string; body: string; id:number}[] = [];
 
   saveNote(note: string[]) {
     let newNote = {title: note[0], body: note[1], id: Date.now() }; // for now just timestamp as id
     this.notes.unshift(newNote);
-    this.saveDataInLocalStorage();
+    this.saveLocalData();
   }
 
-  moveToTrash(note: any[]) {
+  moveToTrash(note: [noteTitle: string, noteID: number]) { // title not actualy necessary
+    //let noteTitle = note[0];
     console.log(note);
-    // filter correct note from notes
-    // push into this.trash & remove from notes
-    // update local storage
+    let noteID = note[1];
+    let targetIndex = this.notes.findIndex( el => {
+      return el.id === noteID; // && el.title === noteTitle
+    });
+    this.trash.unshift(this.notes[targetIndex]);
+    this.notes.splice(targetIndex,1);
+    this.saveLocalData();
+  }
+
+  deleteNote(note: any[]){
+    let targetID: number; // == note[0]
+    this.trash = this.trash.filter( el=> {
+      return !(el.id === targetID);
+    });
+
   }
 
   // save data into local storage
-  saveDataInLocalStorage() {
+  saveLocalData() {
     let notesAsString = JSON.stringify(this.notes);
     let trashAsString = JSON.stringify(this.trash);
     localStorage.setItem('notes', notesAsString);
@@ -37,7 +50,7 @@ export class NotesDashboardComponent implements OnInit {
   }
 
   // load data from local storage
-  loadDataFromLocalStorage() {
+  loadLocalData() {
     // get as strings
     let notesAsString = localStorage.getItem('notes');
     let trashAsString = localStorage.getItem('trash');
@@ -49,7 +62,7 @@ export class NotesDashboardComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void { 
-    this.loadDataFromLocalStorage();
+    this.loadLocalData();
   }
 
 
