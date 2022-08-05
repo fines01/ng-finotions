214588b0b6cd289c318de7e0ed662b093cb9f8f1
I. Fritsch <ines.fritsch@gmail.com>
2022-08-05 13:11:13 +0200

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotesDashboardComponent } from './notes-dashboard/notes-dashboard.component';
+import { TrashDashboardComponent } from './trash-dashboard/trash-dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: NotesDashboardComponent },
+  { path:'trash', component: TrashDashboardComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
