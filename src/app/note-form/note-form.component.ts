0f61bd9ba0d1d169690b808608c0134b97cdcd29
I. Fritsch <ines.fritsch@gmail.com>
2022-08-05 18:31:37 +0200

    validate empty inputs

--- src/app/note-form/note-form.component.ts
+++ src/app/note-form/note-form.component.ts
@@ -9,6 +9,8 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 export class NoteFormComponent implements OnInit {
   
   showForm:boolean = false;
+  titleIsEmpty: boolean = false;
+  bodyIsEmpty: boolean = false;
   newTitle!: string;
   newBody!: string;
 
@@ -19,9 +21,14 @@ export class NoteFormComponent implements OnInit {
   }
 
   onSaveNote() {
-    // TODO validate inputs
-    this.submitNote.emit([this.newTitle, this.newBody]);
-    this.clearInputs();
+    if (this.validateInputs()) {
+      this.submitNote.emit([this.newTitle, this.newBody]);
+      this.clearInputs();
+    }
+    else {
+      this.removeBlinkAnimation();
+      console.log('vaidation failed')
+    }
   }
 
   // I could save new note in local storage here instead of emitting data into notes dashboard
@@ -29,6 +36,24 @@ export class NoteFormComponent implements OnInit {
   clearInputs() {
     this.newTitle = '';
     this.newBody = '';
+  }
+
+  validateInputs(){
+    this.inputIsEmpty(this.newTitle) ? this.titleIsEmpty = true : this.titleIsEmpty = false;
+    this.inputIsEmpty(this.newBody) ? this.bodyIsEmpty = true : this.bodyIsEmpty = false;
+    if (this.titleIsEmpty || this. bodyIsEmpty ) return false;
+    else return true;
+  }
+  
+  inputIsEmpty(value: string | undefined){
+    return (value === undefined || value.trim().length === 0);
+  }
+  
+  removeBlinkAnimation() {
+    setTimeout(()=> {
+      this.titleIsEmpty = false;
+      this.bodyIsEmpty = false;
+    }, 600);
   }
 
   constructor() { }
